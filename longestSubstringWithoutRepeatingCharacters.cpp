2a0fd72ac0dec9048ddbbce6cbe3f9397c1ac2c7
RushYourMind <logitechmx310@gmail.com>
2015-07-04 22:42:52 +0800

    update
    added a version not use map

--- longestSubstringWithoutRepeatingCharacters.cpp
+++ longestSubstringWithoutRepeatingCharacters.cpp
@@ -58,7 +58,27 @@ int Solution3::lengthOfLongestSubstring2(string s)
 			maxLen = i - lastRepeatPosition;
 		myMap[s[i]] = i;//每次都更新字母的index，重复的字母的index会被后面出现的字母所覆盖
 
-		++i;
+		++i;//
+	}
+	return maxLen;
+}
+
+//没有使用map的版本
+int lengthOfLongestSubstring(string s) {
+	const int MAX_CHARS = 256;
+	int m[MAX_CHARS];
+	memset(m, -1, sizeof(m));
+
+	int maxLen = 0;
+	int lastRepeatPos = -1;
+	for (int i = 0; i<s.size(); i++) {
+		if (m[s[i]] != -1 && lastRepeatPos < m[s[i]]) {
+			lastRepeatPos = m[s[i]];
+		}
+		if (i - lastRepeatPos > maxLen) {
+			maxLen = i - lastRepeatPos;
+		}
+		m[s[i]] = i;
 	}
 	return maxLen;
 }
